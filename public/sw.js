422833638d442ecebc4115695fd28407fdf2c140
leonardodinizdasilveira-byte <leonardodinizdasilveira@gmail.com>
2026-05-23 16:07:09 -0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'maeguia-df-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/src/main.jsx',
+  '/src/MaeGuiaDF.jsx',
+  '/src/index.css'
+];
+
+// Instalar o service worker e fazer cache dos arquivos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Buscar do cache quando offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Atualizar o service worker removendo caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Removendo cache antigo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
