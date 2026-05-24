053b02fdba0964f9ad8be84d0337fc3a840ed8be
leonardodinizdasilveira-byte <leonardodinizdasilveira@gmail.com>
2026-05-24 16:52:04 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MaeGuiaDF from "./MaeGuiaDF.jsx";
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MaeGuiaDF />
+    <App />
   </React.StrictMode>
 );
